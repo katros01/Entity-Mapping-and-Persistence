f5d8e0cfb4e5f8e6426cae0fcbe8e2e9bd30ee75
= <=>
2024-08-28 23:44:29 +0200

    add caching and transaction management

--- src/main/java/com/gtp2/entityMapping/services/DepartmentService.java
+++ src/main/java/com/gtp2/entityMapping/services/DepartmentService.java
@@ -6,8 +6,14 @@ import com.gtp2.entityMapping.repositories.DepartmentRepository;
 import com.gtp2.entityMapping.repositories.DoctorRepository;
 import com.gtp2.entityMapping.utils.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import com.gtp2.entityMapping.entities.Department;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.List;
@@ -22,15 +28,19 @@ public class DepartmentService {
     @Autowired
     private DoctorRepository doctorRepository;
 
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS, isolation = Isolation.READ_COMMITTED)
+    @Cacheable(value = "departments")
     public List<Department> getAllDepartments() {
         return departmentRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
+    @Cacheable(value = "departments", key = "#id")
     public Optional<Department> getDepartmentById(String id) {
         return departmentRepository.findById(id);
     }
 
-
+    @Transactional(propagation = Propagation.REQUIRED)
     public Department createDepartment(DepartmentDto departmentRequest) {
         Doctor director = doctorRepository.findById(departmentRequest.getDirectorId())
                 .orElseThrow(() -> new ResourceNotFoundException("Doctor not found"));
@@ -44,6 +54,8 @@ public class DepartmentService {
         return departmentRepository.save(department);
     }
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.SERIALIZABLE)
+    @CachePut(value = "departments", key = "#id")
     public Department updateDepartment(String id, DepartmentDto departmentRequest) {
         Department department = departmentRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Department not found"));
@@ -59,6 +71,8 @@ public class DepartmentService {
         return departmentRepository.save(department);
     }
 
+    @Transactional
+    @CacheEvict(value = "departments", key = "#id")
     public void deleteDepartment(String id) {
         departmentRepository.deleteById(id);
     }
--- src/main/java/com/gtp2/entityMapping/services/DoctorService.java
+++ src/main/java/com/gtp2/entityMapping/services/DoctorService.java
@@ -2,8 +2,13 @@ package com.gtp2.entityMapping.services;
 
 import com.gtp2.entityMapping.repositories.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import com.gtp2.entityMapping.entities.Doctor;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,18 +19,25 @@ public class DoctorService {
     @Autowired
     private DoctorRepository doctorRepository;
 
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    @Cacheable(value = "doctors")
     public List<Doctor> getAllDoctors() {
         return doctorRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
+    @Cacheable(value = "doctors", key = "#id")
     public Optional<Doctor> getDoctorById(String id) {
         return doctorRepository.findById(id);
     }
 
+    @Transactional
     public Doctor createDoctor(Doctor doctor) {
         return doctorRepository.save(doctor);
     }
 
+    @Transactional
+    @CachePut(value = "doctors", key = "#id")
     public Optional<Doctor> updateDoctor(String id, Doctor doctorDetails) {
         return doctorRepository.findById(id)
                 .map(doctor -> {
@@ -39,10 +51,14 @@ public class DoctorService {
                 });
     }
 
+    @Transactional
+    @CacheEvict(value = "doctors", key = "#id")
     public void deleteDoctor(String id) {
         doctorRepository.deleteById(id);
     }
 
+    @Transactional(readOnly = true)
+    @Cacheable(value = "doctors", key = "#speciality")
     public List<Doctor> findBySpeciality(String speciality) {
         return doctorRepository.findBySpeciality(speciality);
     }
